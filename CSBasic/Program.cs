847de59808b4137401aa7cac5ee8da8ee0352cf9
Solmin Kim <solminkim03@naver.com>
2021-04-16 10:21:27 +0900

    #2 기본용어에 대한 실습   (이모지 변수명이 안됨)

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,17 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            var name = "김" + "솔" + "민";
+            Console.Write("개행이 안되는 친구");
+            Console.WriteLine("개행이 되는 친구");
+
+            // string do= "안되는 거";
+            var alpha = "알파";
+            var 한글변수명 = "안녕";
+            // 이모는 변수명으로 안됨.
+
+            // 인라인 주석
+
         }
     }
 }
