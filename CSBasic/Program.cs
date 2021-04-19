7957a8f4c42231306e1b3a3d430b3e128fc616d1
Solmin Kim <solminkim03@naver.com>
2021-04-19 15:27:04 +0900

    #4 기본 자료형 실습    (실수하기 쉬움 주의!)

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -23,8 +23,67 @@ namespace CSBasic
 
             // 출력
             Console.WriteLine("개행이 되는 출력");
-            Console.Write("개행이 안되는 출력");
+          /*  Console.Write("개행이 안되는 출력");*/
 
+            // 정수 자료형
+            Console.WriteLine(52);   // 기본 자료형은 정수형
+            // Arithmetic Operator +, - ,*, / 
+            Console.WriteLine(52 + 300);
+            //연산자 우선순위 수학과 비슷 (Operator Priority)
+            Console.WriteLine(5 + 3 * 2);
+
+            Console.WriteLine(10 / 5);
+            Console.WriteLine(10 % 5);
+            Console.WriteLine(7%3);
+
+            // Real Number
+            Console.WriteLine(52.276);
+
+            Console.WriteLine(0);
+            Console.WriteLine(0.0);
+
+            Console.WriteLine(1.0 + 2.5);
+            Console.WriteLine(1.0 - 2.5);
+            Console.WriteLine(1.0 * 2.5);
+            Console.WriteLine(1.0 / 2.5);
+
+            Console.WriteLine(1.0 / 2.0);       // 소수점 나옴
+            Console.WriteLine(1 / 2);
+            // 소수가 나옴. (암묵적 형변환)
+            Console.WriteLine(1.0 / 2);
+            Console.WriteLine(1 / 2.0);
+
+            Console.WriteLine('A');
+            Console.WriteLine('가');
+            /*Console.WriteLine('BB');*/
+            Console.WriteLine("AB");
+
+            // Escape Character
+            Console.WriteLine("미림\t여자정보\t과학고");     // 4개 기준 
+            Console.WriteLine("미림여자정보과학고\t안녕");
+            Console.WriteLine("\\ 역슬래시 \n 엔터문자 \"큰따옴표");
+
+            // 문자열 연결 연산자 Concatenation Operator
+            Console.WriteLine("A" + "B");
+            Console.WriteLine("A" + 1);
+            Console.WriteLine(1 + "A");
+            Console.WriteLine("1" + "1");
+            Console.WriteLine('A' + 'B');
+            Console.WriteLine('A' - 'B');
+            /* Console.WriteLine("A" - "B");*/
+
+            Console.WriteLine("안녕하세요"[0]);
+            /*Console.WriteLin
+             * e("안녕하세요"[-1]);*/
+
+            // boll/ boolean
+            Console.WriteLine(true);
+            Console.WriteLine(false);   // 출력은 앞메 대문자
+
+            //Comparison operator
+            // == 1.!= <, >= 
+            Console.WriteLine(62 < 273);
+            Console.WriteLine(DateTime.Now.Hour<3 || 8<DateTime.Now.Hour);
         }
     }
 }
