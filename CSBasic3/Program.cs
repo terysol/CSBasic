51624b24c3a3c96a06b28ae5a1e0a3988abe712e
Solmin Kim <solminkim03@naver.com>
2021-05-03 14:56:59 +0900

    #17 반복문과 배열

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -10,6 +10,19 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
+            int [] intArray ={ 52,172,13,13,103}];
+            intArray[2] = 0;
+            
+            Console.WriteLine(intArray[1]);
+            Console.WriteLine(intArray[2]);
+            Console.WriteLine(intArray[3]);
+            Console.WriteLine(intArray[4]);
+            Console.WriteLine(intArray[5]);
+            Console.WriteLine(intArray.Length);
+
+            int[] intArray2 = new int[100];
+            Console.WriteLine(intArray2[0]);
+            Console.WriteLine(intArray2[999]);
         }
     }
 }
