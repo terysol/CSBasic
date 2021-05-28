ef04d2ec83b530eebc8cd5b58db6267dc382e1c8
solmin <solminkim03@naver.com>
2021-05-28 10:16:37 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        /// 용도별로 나눠서 클래스를 쪼갤 수 있다. partial
+    }
+}
