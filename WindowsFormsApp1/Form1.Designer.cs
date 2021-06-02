0164f435ec0c66b56809bb34ff025aa4c3d3ae32
solmin <solminkim03@naver.com>
2021-06-02 10:11:08 +0900

    #22 오토캣다루기와 윈도폼 실습하기

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,21 +30,58 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.button1.Font = new System.Drawing.Font("굴림", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(111, 242);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "눌러주세요.";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(53, 12);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(238, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(401, 446);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(334, 309);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Location = new System.Drawing.Point(200, 200);
             this.Name = "FormMain";
             this.Text = "즐거운 금요일 ~";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,37 @@ namespace WindowsFormsApp1
         public FormMain()
         {
             InitializeComponent();
+
+            // this.Text = "핀과 제이크 버전 옥토캣 짜잔";
+            Button button = new Button();
+            button.Text = "버튼";
+            button.Location = new Point(13, 13 + 23 + 23);
+            button.Width = 100;
+            Controls.Add(button);
+
+            Button button2 = new Button();
+            button2.Text = "버튼1";
+            button2.Location = new Point(13, 13 + 23 + 23);
+            button2.Width = 150;
+            Controls.Add(button2);
+
+            Button buttonTemp;
+            for(int i = 0; i < 5; i++)
+            {
+                buttonTemp = new Button();
+                buttonTemp.Text = "버튼" + i;
+                buttonTemp.Location = new Point(13, 13+23 + 23 + 20 * i);
+                buttonTemp.Width = 100 + i * 20;
+                Controls.Add(buttonTemp);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value < 20)
+                trackBar1.Value = 20;
+            
+            this.Opacity = trackBar1.Value / 100.0;
         }
     }
 }
