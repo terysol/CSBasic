3e2285e1be42f8a92cbcc2ae52cc68506f94e21a
solmin <solminkim03@naver.com>
2021-06-14 15:37:11 +0900

    #27 접근제한자 (c#에서는 기본 접근제한자가 private)

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -15,26 +15,48 @@ namespace CSBasic5
             Console.WriteLine(ml.Multi(103, 32));
             ml.Print();
             Console.WriteLine(ml.Sum(1, 100));
-            Console.WriteLine(MyMath.Abs(52));
-            Console.WriteLine(MyMath.Abs(273));
+            Console.WriteLine((52));
+            Console.WriteLine((273));
+            Console.WriteLine((52.273));
+
+            ml.somePublic();
+            
+            
         }
         class MyMath
         {
-            public static int Abs(int input)
+            public static void Abs(int input)
             {
-                /* if(input <0)
-                 {
-                     return -input;
-                 }
-                 else
-                 {
-                     return input;
-                 }*/
-                return (input < 0) ? -input : input;
+                Console.WriteLine("정수 : " + ((input < 0) ? -input:input));
+                // return (input < 0) ? -input : input;
+            }
+
+            public static void Abs(double input)
+            {
+                //  return (input < 0) ? -input : input;
+                Console.WriteLine("정수 : " + ((input < 0) ? -input:input));
+            }
+            public static void Abs(long input)
+            {
+                Console.WriteLine("정수 : " + ((input < 0) ? -input:input));
+                //
             }
         }
         class MultiClass
         {
+            public void somePublic()
+            {
+                Console.WriteLine("public !");
+            }
+            void someDefault()      // C#은 DEFAULT가 없다. -> private  (시험에 나옴)
+            {
+                Console.WriteLine("default !");
+            }
+            private void somePrivate()
+            {
+                Console.WriteLine("private!");
+            }
+
             // 'Test'를 class에 포함하지 않기 
             public int Multi(int x, int y)
             {
